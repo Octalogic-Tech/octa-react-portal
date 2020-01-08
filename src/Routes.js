cffd545ef9fdac027d73eaa001b10dc8ffde9ce1
ajay <ajay@octalogic.in>
2020-01-08 17:21:45 +0530

    handle server error

--- src/Routes.js
+++ src/Routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import PortfolioContainer from "./containers/PortfolioContainer";
 import Landing from "./pages/Landing";
 import NotFound from "./pages/NotFound";
+import ServerError from "./pages/ServerError";
 import Loading from "./pages/Loading";
 
 const Routes = ({ analytics, toggleTheme, currentTheme }) => (
@@ -18,6 +19,7 @@ const Routes = ({ analytics, toggleTheme, currentTheme }) => (
       </Route>
       <Route exact analytics={analytics} path="/" component={Landing} />
       <Route exact path="/testPage" component={Loading} />
+      <Route exact path="/server_error" component={ServerError} />
       <Route component={NotFound} />
     </Switch>
   </Router>
---
+++ src/pages/ServerError/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ServerError = props => {
+  return <p>500 Internal Server Error</p>;
+};
+
+export default ServerError;
